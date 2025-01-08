465b0ae4dc67fa26333be89c13025ec0c36547ec
Kirill Tobola <kirilltobola@gmail.com>
2025-01-09 00:33:32 +0800

    return model output instead of loss

--- model/model.py
+++ model/model.py
@@ -2,7 +2,6 @@ import torch
 import torch.nn as nn
 from transformers import AutoConfig, AutoModel
 from config import Config
-from model.loss import nt_xent_loss
 
 
 class Colem(nn.Module):
@@ -33,17 +32,17 @@ class Colem(nn.Module):
         """CoLeM forward pass.
 
         Args:
-            input (torch.Tensor): batch(2*batch_size, sequence_length) of columns
+            input (torch.Tensor): batch(2 * batch_size, sequence_length) of columns
 
         Returns:
-            torch.Tensor: NT-Xent loss
+            torch.Tensor: Model output
         """
         encoder_last_hidden_state = self.encoder(input)[0]  # (2*batch_size, sequence_length, encoder_output)
         projector_output = self.projector(encoder_last_hidden_state)  # (2*batch_size, sequence_length, reduced_output)
 
         # Get column representations, i.e. encoder [CLS] token positions.
-        output = projector_output[:, 0, :]  # (2*batch_size, reduced_output)
-        return nt_xent_loss(output)
+        output = projector_output[:, 0, :]  # (2 * batch_size, reduced_output)
+        return output
 
 
 if __name__ == "__main__":
@@ -54,7 +53,7 @@ if __name__ == "__main__":
     # Test forward pass
     torch.manual_seed(42)
 
-    x = torch.randint(0, 30000, (2 * 32, 512))
+    x = torch.randint(0, 3, (2 * 32, 512))  # batch_size = 32 ; output = 512
     print(x.shape)
 
     loss = model(x)
