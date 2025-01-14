f859b5f72766cdc15ff10bb3bf2e0b3dd341e340
kirilltobola <kirilltobola@gmail.com>
2025-01-14 15:56:42 +0800

    remove redundant code and update docstrings

--- dataset/table_dataloader.py
+++ dataset/table_dataloader.py
@@ -2,11 +2,11 @@ from typing import Optional
 
 from torch.utils.data import DataLoader
 
-from dataset.dataset import TableDataset
+from dataset.table_dataset import TableDataset
 
 
 class TableDataLoader(DataLoader):
-    """Data loader.
+    """Table data loader.
 
     Provides an iterable over the given dataset.
 
@@ -14,16 +14,16 @@ class TableDataLoader(DataLoader):
         dataset: dataset from which to load the data.
         batch_size: how many samples per batch to load.
         shuffle: perform shuffling of dataset.
-        num_workers: how many subprocesses to use for data loading.
         collate_fn: merges a list of samples to form a mini-batch of Tensors.
+        num_workers: how many subprocesses to use for data loading.
     """
     def __init__(
         self,
         dataset: TableDataset,
         batch_size: int,
-        shuffle: bool,
+        shuffle: bool = True,
+        collate_fn: Optional[callable] = None,
         num_workers: Optional[int] = 0,
-        collate_fn: Optional[callable] = None
     ):
         self.init_kwargs = {
             'dataset': dataset,
@@ -33,7 +33,3 @@ class TableDataLoader(DataLoader):
             'num_workers': num_workers
         }
         super().__init__(**self.init_kwargs)
-
-
-if __name__ == "__main__":
-    pass
--- dataset/table_dataset.py
+++ dataset/table_dataset.py
@@ -11,30 +11,32 @@ class TableDataset(Dataset):
 
     Args:
         data_dir: path to directory, where dataset .csv files placed.
+        sep: csv separator character.
         num_rows: amount of how many rows to read per .csv file, if None read all rows.
+        file_name: name of csv file, if dataset contains only one file.
         transform: Optional transform to be applied on a sample
         target_transform: Optional transform to be applied on a target.
     """
     def __init__(
             self,
             data_dir: str,
-            num_rows: Optional[int],
+            sep: str = "⇧",
+            num_rows: Optional[int] = None,
             file_name=None,
             transform=None,
             target_transform=None,
     ):
-        # Read dataset .csv files
         if file_name:
             self.df = pd.read_csv(
                 data_dir + file_name,
-                sep="⇧",
+                sep=sep,
                 engine="python",
                 quotechar='"',
                 on_bad_lines="warn",
-                nrows=num_rows if num_rows is not None else None
+                nrows=num_rows
             )
         else:
-            self.df = self.read_multiple_csv(data_dir, num_rows)
+            self.df = self.read_multiple_csv(data_dir, sep, num_rows=num_rows)
 
         self.transform = transform
         self.target_transform = target_transform
@@ -43,13 +45,9 @@ class TableDataset(Dataset):
         return len(self.df)
 
     def __getitem__(self, idx):
-        return {
-            "table_id": self.df.iloc[idx]["table_id"],
-            "header": self.df.iloc[idx]["column_header"],
-            "data": self.df.iloc[idx]["column_data"],
-        }
+        return self.df.iloc[idx]["column_data"]
 
-    def read_multiple_csv(self, data_dir: str, num_rows: Optional[int] = None) -> pd.DataFrame:
+    def read_multiple_csv(self, data_dir: str, sep: str, prefix: str = "data_", num_rows: Optional[int] = None) -> pd.DataFrame:
         """Read dataframe from multiple csv files.
 
         If dataset was split into multiple files, it will be concatenated. Dataset is stored
@@ -57,6 +55,8 @@ class TableDataset(Dataset):
 
         Args:
             data_dir: path to directory, where dataset .csv files placed.
+            sep: csv separator character.
+            prefix: filename prefix for dataset files.
             num_rows: amount of how many rows to read per .csv file, if None read all rows.
 
         Returns:
@@ -64,30 +64,17 @@ class TableDataset(Dataset):
         """
 
         df_list = []
-        num_chunks = len(glob.glob(data_dir + "data_*.csv"))
-        if num_chunks > 1:
-            for i in range(num_chunks):
-                df = pd.read_csv(
-                    data_dir + f"data_{i}.csv",
-                    sep="⇧", # TODO: move me to config!
-                    engine="python",
-                    quotechar='"',
-                    on_bad_lines="warn",
-                    nrows=num_rows if num_rows is not None else None
-                )
-                df_list.append(df)
-            return pd.concat(df_list, axis=0)
-        
-        return pd.read_csv(
-            data_dir + "data.csv",
-            sep="⇧",
-            engine="python",
-            quotechar='"',
-            on_bad_lines="warn",
-            nrows=num_rows if num_rows is not None else None
-        )
+        chunks = glob.glob(data_dir + f"{prefix}*.csv")
+        assert len(chunks) > 1
 
-
-if __name__ == "__main__":
-    pass
-    # TODO
+        for filename in chunks:
+            df = pd.read_csv(
+                filename,
+                sep=sep,
+                engine="python",
+                quotechar='"',
+                on_bad_lines="warn",
+                nrows=num_rows
+            )
+            df_list.append(df)
+        return pd.concat(df_list, axis=0)
